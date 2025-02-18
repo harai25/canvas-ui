f08ac4ab2fc934f7ff235c341a114e9f54f5a49a
Harai25 <gundorov2001@gmail.com>
2025-02-18 13:15:38 +0400

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import Terminal from 'vite-plugin-terminal'
 
 export default defineConfig({
-  base: 'canvas-ui',
+  base: '/canvas-ui/',
   plugins: [
     Terminal()
   ],
